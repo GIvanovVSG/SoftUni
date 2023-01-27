cd71a08e9777348ae9424ad58b4336a3dc86f5f3
VSG\GIvanov <GIvanov@vsgbg.com>
2023-01-27 17:49:49 +0200

    While Loop

---
+++ While-Loop - More Exercises/01. Dishwasher/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01._Dishwasher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottles = int.Parse(Console.ReadLine()) * 750;
+            int dish = 5;
+            int pot = 15;
+            string end;
+            int quantity;
+            int washTimes = 0;
+            int cleanDishes = 0;
+            int cleanPots = 0;
+
+            while ((end = Console.ReadLine()) != "End")
+            {
+                quantity = int.Parse(end);
+                washTimes++;
+
+                if(washTimes == 3)
+                {
+                    bottles -= quantity * pot;
+                    washTimes = 0;
+                    cleanPots += quantity;
+                }
+                else
+                {
+                    bottles-= quantity * dish;
+                    cleanDishes += quantity;
+                }
+
+                if(bottles < 0)
+                {
+                    Console.WriteLine($"Not enough detergent, {Math.Abs(bottles)} ml. more necessary!");
+                    break;
+                }
+            }
+
+            if(end == "End")
+            {
+                Console.WriteLine("Detergent was enough!");
+                Console.WriteLine($"{cleanDishes} dishes and {cleanPots} pots were washed.");
+                Console.WriteLine($"Leftover detergent {bottles} ml.");
+            }
+        }
+    }
+}
---
+++ While-Loop - More Exercises/02. Report System/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _02._Report_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = int.Parse(Console.ReadLine());
+            string end;
+            int transaction;
+            int cashCount = 0;
+            int cardCount = 0;
+            double cash = 0;
+            double card = 0;
+            int count = 0;
+
+            while ((end = Console.ReadLine()) != "End")
+            {
+                transaction = int.Parse(end);
+                count++;
+
+                if (count % 2 == 1)
+                {
+                    if (transaction > 100)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        cash += transaction;
+                        cashCount++;
+                    }
+                }
+                else
+                {
+                    if (transaction < 10)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        card += transaction;
+                        cardCount++;
+                    }
+                }
+
+                if (sum <= cash + card)
+                {
+                    Console.WriteLine($"Average CS: {(cash / cashCount).ToString("0.00")}");
+                    Console.WriteLine($"Average CC: {(card / cardCount).ToString("0.00")}");
+                    break;
+                }
+            }
+
+
+            if (end == "End")
+            {
+                Console.WriteLine("Failed to collect required money for charity.");
+            }
+        }
+    }
+}
