878e5c72baf848f0a81315eeec94f87465dda60e
VSG\GIvanov <GIvanov@vsgbg.com>
2023-01-31 11:41:07 +0200

    While loop-More Exercises

---
+++ While-Loop - More Exercises/05. Average Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05._Average_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            int count = 0;
+
+            while (count < n)
+            {
+                count++;
+                int number = int.Parse(Console.ReadLine());
+
+                sum += number;
+            }
+            
+
+            Console.WriteLine((sum / n).ToString("0.00"));
+        }
+    }
+}
