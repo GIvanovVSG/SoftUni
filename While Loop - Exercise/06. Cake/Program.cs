057a2f3c088f8bf116b181320813c22a0d3b2cf8
VSG\GIvanov <GIvanov@vsgbg.com>
2023-02-16 09:21:19 +0200

    While loop- 06. Cake

--- While Loop - Exercise/06. Cake/Program.cs
+++ While Loop - Exercise/06. Cake/Program.cs
@@ -6,7 +6,28 @@ namespace _06._Cake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            string stop;
+            int pieces = width * height;
+            int piecesToGet;
+
+            while((stop = Console.ReadLine()) != "STOP")
+            {
+                piecesToGet = int.Parse(stop);
+                pieces -= piecesToGet;
+                
+                if(pieces <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(pieces)} pieces more.");
+                    break;
+                }
+            }
+
+            if(stop == "STOP")
+            {
+                Console.WriteLine($"{pieces} pieces are left.");
+            }
         }
     }
 }
