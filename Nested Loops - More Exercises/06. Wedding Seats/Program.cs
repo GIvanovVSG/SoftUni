0c1dbeec7e04d803f7e406dc4ca9230497b4030d
VSG\GIvanov <GIvanov@vsgbg.com>
2023-02-17 16:59:09 +0200

    Wedding seats

--- Nested Loops - More Exercises/06. Wedding Seats/Program.cs
+++ Nested Loops - More Exercises/06. Wedding Seats/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _06._Wedding_Seats
 {
@@ -6,7 +7,37 @@ namespace _06._Wedding_Seats
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char lastSector = char.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            int seats = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (char sector = 'A'; sector <= lastSector; sector++)
+            {
+                for (int row = 1; row <= rows; row++)
+                {
+                    if(row % 2 == 0)
+                    {
+                        for (int seat = 1; seat <= seats + 2; seat++)
+                        {
+                            Console.WriteLine($"{sector}{row}{Convert.ToChar(seat + 96)}");
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        for (int seat = 1; seat <= seats; seat++)
+                        {
+                            Console.WriteLine($"{sector}{row}{Convert.ToChar(seat + 96)}");
+                            count++;
+                        }
+                    }
+                    
+                }
+                rows++;
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
